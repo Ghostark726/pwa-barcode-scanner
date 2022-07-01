01c7531a5096d41eb5fdb08034c46a1c907c2bae
Ghostark726 <86778707+Ghostark726@users.noreply.github.com>
2022-07-01 11:39:51 -0500

    Update productNotFound.js

--- src/components/productNotFound/productNotFound.js
+++ src/components/productNotFound/productNotFound.js
@@ -12,7 +12,7 @@ const ProductNotFound = (props) => {
 
     return (<div className="productNotFound__container">
       <h2 className="productDisplay__title not-found">Product not found <span role="img" aria-label="crying emoji">😢</span></h2>
-      <p>The barcode: {code} gave no results!</p>
+      <p>Searching the barcode {code} for excess results!</p>
       <p>You can also enter the barcode below and try again:</p>
       <BarcodeInputField />
       <AddProductInfo />
