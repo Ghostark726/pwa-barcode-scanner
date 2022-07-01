aee4ad7022d863f2bdda2146af383d7d606239d4
Ghostark726 <86778707+Ghostark726@users.noreply.github.com>
2022-07-01 12:01:22 -0500

    Update productNotFound.js

--- src/components/productNotFound/productNotFound.js
+++ src/components/productNotFound/productNotFound.js
@@ -12,7 +12,7 @@ const ProductNotFound = (props) => {
   window.open("https://info-handler.copycrunch1.repl.co/v1/"+code)
   
     return (<div className="productNotFound__container">
-      <h2 className="productDisplay__title not-found">Product not found <span role="img" aria-label="crying emoji">😢</span></h2>
+      <h2 className="productDisplay__title not-found">Still searching..<span role="img" aria-label="crying emoji">😢</span></h2>
       <p>Searching the barcode {code} for excess results!</p>
       <p>You can also enter the barcode below and try again:</p>
       <BarcodeInputField />
